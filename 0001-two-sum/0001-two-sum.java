a907d99c9b372d0b527cb0a49d6398d1c8197213
Uday Sharma <uday89750@gmail.com>
2026-06-26 16:21:47 +0530

    Time: 44 ms (32.88%), Space: 46.9 MB (72.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,10 +3,8 @@ class Solution {
         int [] sum= new int[2];
         for(int i = 0 ;i<nums.length;i++){
             
-            for(int j = 0;j<nums.length;j++){
-                if(i==j){
-                    continue;
-                }
+            for(int j = i+1;j<nums.length;j++){
+                
                 if(nums[i]+nums[j]==target){
                     sum[0]=i;
                     sum[1]=j;
