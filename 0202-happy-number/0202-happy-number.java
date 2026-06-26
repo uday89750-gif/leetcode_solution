e330ec07a0a30054a0ca4b2d053396dae8aecb60
Uday Sharma <uday89750@gmail.com>
2026-06-26 12:28:46 +0530

    Time: 1 ms (78.11%), Space: 42.4 MB (66.26%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -21,8 +21,8 @@ class Solution {
        int sum = 0 ;
        while(n!=0){
         int digit = n%10;
-        sum = sum + digit*digit;
-        n=n/10;
+        sum += digit*digit;
+        n/=10;
        }
        return sum ;
 
