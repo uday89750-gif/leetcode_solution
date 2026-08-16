87068bb9c346b3f9d3bc56083e1545784a291aa0
Uday Sharma <uday89750@gmail.com>
2026-08-17 00:40:44 +0530

    Time: 973 ms (5.16%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,6 +1,3 @@
 # Write your MySQL query statement below
-select c.name as Customers 
-from Customers c 
-left join Orders o
-on c.ID=O.customerID
-where customerId is null
+select c.name as Customers from Customers c left join Orders o
+on c.ID=o.customerID where customerId is null
